45921152166656ac4ab79313baef8fd7ee2eb166
apoorva2014 <mahaja2@rpi.edu>
2015-11-02 22:58:06 -0500

    adds templates

--- views.py
+++ views.py
@@ -1,5 +1,47 @@
+from flask import render_template, request
+from models import Category, Priority, Todo, db
 from todoapp import app
 
 @app.route('/')
-def index():
-    return 'hello world!'
+def list_all():
+    return render_template(
+    list.html,
+    categories=Category.query.all(),
+    todos=Todo.query.join(Priority).order_by(Priority.value.desc()))
+
+@app.route('/new-task', methods=['POST'])
+def new():
+    if request.method == 'POST':
+        category = Category.query.filter_by(id=request.form['category']).first()
+        priority = Priority.query.filter_by(id=request.form['priority']).first()
+        todo = Task(category, priority, request.form['description'])
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template(
+        'new-task.html',
+        page='new-task',
+        categories=Category.query.all()
+        priorities=Priority.query.all()
+        )
+
+@app.route('/<int:todo_id>', methods=['GET', 'POST'])
+def update_todo(todo_id):
+   todo = Task.query.get(todo_id)
+   if request.method == 'GET':
+       return render_template(
+           'new-task.html',
+           todo=todo,
+           categories=Category.query.all(),
+           priorities=Priority.query.all()
+       )
+   else:
+       category = Category.query.filter_by(id=request.form['category']).first()
+       priority = Priority.query.filter_by(id=request.form['priority']).first()
+       description = request.form['description']
+       todo.category = category
+       todo.priority = priority
+       todo.description = description
+       db.session.commit()
+       return redirect('/')
